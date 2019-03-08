3801cc676cbab717069a3229fe069cc81b785ce0
Bacchus Jackson <bjjackson32@gmail.com>
2019-03-08 17:20:13 -0500

    added navbar and routing

--- cure-app/src/app/app-routing.module.ts
+++ cure-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewEntryComponent } from './components/new-entry/new-entry.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'new', component: NewEntryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cure-app/src/app/app.module.ts
+++ cure-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NewEntryComponent } from './components/new-entry/new-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NewEntryComponent
   ],
   imports: [
     BrowserModule,
