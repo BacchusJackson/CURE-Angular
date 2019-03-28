362ff83f772067ee7007bb0135b11490676a1647
Bacchus Jackson <bjjackson32@gmail.com>
2019-03-28 15:53:48 -0400

    added mongoose database connection

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //database: 'mongodb://ec2-52-61-191-99.us-gov-west-1.compute.amazonaws.com:27017',
+    database: 'mongodb://localhost:27017',
+    secret: 'yoursecret'
+}
--- server/server.js
+++ server/server.js
@@ -4,8 +4,24 @@ const path = require ('path');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('../config/database');
+
+// Connect to database in config file
+mongoose.connect(config.database);
+
+//success connection message
+mongoose.connection.on('connected', () => {
+    console.log('connected to database ' + config.database);
+});
+
+//on error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+err)
+});
 
 const app = express();
+
+const users = require('./routes/users');
 
 //sets the port to an enviromental variable or 3000
 const port = process.env.PORT || 3000;
@@ -17,15 +33,18 @@ app.set('port', port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/users', users);
+
 //points to the folder where angular compiles the html code
 app.use(express.static(path.join(__dirname, '../cure-app/dist/cure-app')));
 
 //sets the api route
-app.use('/api', api);
+//app.use('/api', api);
 
 //get any request and return a response
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../cure-app/dist/cure-app/index.html'))
 });
 
+//starts the server
 app.listen(port, () => console.log('Server listening on port: ' + port));
