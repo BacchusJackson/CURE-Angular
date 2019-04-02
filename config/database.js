b25232a10373be7eb3da0c362ca2227989d78a31
Bacchus Jackson <bjjackson32@gmail.com>
2019-04-02 09:29:42 -0400

    added Users database model

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-    //database: 'mongodb://ec2-52-61-191-99.us-gov-west-1.compute.amazonaws.com:27017',
     database: 'mongodb://localhost:27017',
     secret: 'yoursecret'
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/users');
 
 //Register
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER')
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: `${req.body.firstName}.${req.body.lastName}`,
+        password: req.body.password
+
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({success: false, msg:'failed to register user...'});
+        }else{
+        res.json({success: true, msg:'User successfully registered!'});
+        }
+    })
 });
 
 //Authenticate 
@@ -14,10 +32,6 @@ router.get('/authenticate', (req, res, next) => {
 //Profile
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE')
-});
-
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE')
 });
 
 module.exports = router;
